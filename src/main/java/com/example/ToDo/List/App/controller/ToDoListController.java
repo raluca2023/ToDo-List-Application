b9f9c4d44173a5b782634af59693ceb60a21c957
Raluca <d.crosmanraluca@yahoo.com>
2024-01-24 21:21:50 +0200

    Controller

--- src/main/java/com/example/ToDo/List/App/controller/ToDoListController.java
+++ src/main/java/com/example/ToDo/List/App/controller/ToDoListController.java
@@ -7,39 +7,42 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequiredArgsConstructor
-@RequestMapping("todolist")
 public class ToDoListController {
-    private final ToDoListService toDoListService;
+    @Controller
+    @RequiredArgsConstructor
+    @RequestMapping("todolist")
+    public class ToDoController {
+        private final ToDoListService toDoListService;
 
-    @GetMapping("/all")
-    public String getAll (Model model) {
-        model.addAttribute("list", toDoListService.getAll());
-        return "listDir/all";
-    }
+        @GetMapping("/all")
+        public String getAll (Model model) {
+            model.addAttribute("list", toDoListService.getAll());
+            return "listDir/all";
+        }
 
-    @PostMapping("/add")
-    public String saveAll (@ModelAttribute ToDoListModel toDoListModel) {
-        toDoListService.save(toDoListModel);
-        return "redirect:/todolist/all";
-    }
-    @GetMapping("/{id}")
-    public String getOneListItemById (Model model, @PathVariable Long id) {
-        model.addAttribute("oneItem", toDoListService.getOne(id));
-        return "listDir/one";
-    }
+        @PostMapping("/add")
+        public String saveAll (@ModelAttribute ToDoListModel toDoListModel) {
+            toDoListService.save(toDoListModel);
+            return "redirect:/todolist/all";
+        }
+        @GetMapping("/{id}")
+        public String getOneListItemById (Model model, @PathVariable Long id) {
+            model.addAttribute("oneItem", toDoListService.getOne(id));
+            return "listDir/one";
+        }
 
-    @PostMapping("/edit/{id}")
-    public String updateAll (@PathVariable Long id, @ModelAttribute ToDoListModel toDoListModel) {
-        toDoListService.update(id,toDoListModel);
-        return "redirect:/todolist/" + id;
-    }
+        @PostMapping("/edit/{id}")
+        public String updateAll (@PathVariable Long id, @ModelAttribute ToDoListModel toDoListModel) {
+            toDoListService.update(id,toDoListModel);
+            return "redirect:/todolist/" + id;
+        }
 
-    @GetMapping("/delete/{id}")
-    public String deleteItem (@PathVariable Long id) {
-        toDoListService.deleteById(id);
-        return "redirect:/todolist/all";
+        @GetMapping("/delete/{id}")
+        public String deleteItem (@PathVariable Long id) {
+            toDoListService.deleteById(id);
+            return "redirect:/todolist/all";
+        }
+
     }
 
 }
