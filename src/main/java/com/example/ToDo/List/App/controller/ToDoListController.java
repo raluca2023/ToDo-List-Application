305f61b226fd2e820864b02f2adf6db6931958a0
Raluca <d.crosmanraluca@yahoo.com>
2024-01-24 18:33:10 +0200

    Controller

---
+++ src/main/java/com/example/ToDo/List/App/controller/ToDoListController.java
@@ -0,0 +1,45 @@
+package com.example.ToDo.List.App.controller;
+
+import com.example.ToDo.List.App.model.ToDoListModel;
+import com.example.ToDo.List.App.service.ToDoListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("todolist")
+public class ToDoListController {
+    private final ToDoListService toDoListService;
+
+    @GetMapping("/all")
+    public String getAll (Model model) {
+        model.addAttribute("list", toDoListService.getAll());
+        return "listDir/all";
+    }
+
+    @PostMapping("/add")
+    public String saveAll (@ModelAttribute ToDoListModel toDoListModel) {
+        toDoListService.save(toDoListModel);
+        return "redirect:/todolist/all";
+    }
+    @GetMapping("/{id}")
+    public String getOneListItemById (Model model, @PathVariable Long id) {
+        model.addAttribute("oneItem", toDoListService.getOne(id));
+        return "listDir/one";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updateAll (@PathVariable Long id, @ModelAttribute ToDoListModel toDoListModel) {
+        toDoListService.update(id,toDoListModel);
+        return "redirect:/todolist/" + id;
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteItem (@PathVariable Long id) {
+        toDoListService.deleteById(id);
+        return "redirect:/todolist/all";
+    }
+
+}
